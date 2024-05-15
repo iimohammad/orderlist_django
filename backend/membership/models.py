a1af6b28d69ad92c3bf397870a1956db60f905cb
Hossein <hossein16akbari@gmail.com>
2024-05-16 00:36:50 +0330

    Adding price field to membership

--- backend/membership/models.py
+++ backend/membership/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-
 class Membership(models.Model):
 
     """Represents a membership plan"""
@@ -38,3 +37,38 @@ class Membership(models.Model):
         max_length=1,
         choices=MEMBERSHIP_PERIOD_CHOICES,
         default=MONTHLY_PERIOD)
+    
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def save(self, *args, **kwargs):
+        # Calculate price based on membership and period
+        if self.membership == self.MEMBERSHIP_FREE:
+            if self.period == self.MONTHLY_PERIOD:
+                self.price = 0
+            elif self.period == self.QUARTERLY_PERIOD:
+                self.price = 0
+            elif self.period == self.ANNUALLY_PERIOD:
+                self.price = 0
+        elif self.membership == self.MEMBERSHIP_BRONZE:
+            if self.period == self.MONTHLY_PERIOD:
+                self.price = 10
+            elif self.period == self.QUARTERLY_PERIOD:
+                self.price = 25
+            elif self.period == self.ANNUALLY_PERIOD:
+                self.price = 90
+        elif self.membership == self.MEMBERSHIP_SILVER:
+            if self.period == self.MONTHLY_PERIOD:
+                self.price = 20
+            elif self.period == self.QUARTERLY_PERIOD:
+                self.price = 50
+            elif self.period == self.ANNUALLY_PERIOD:
+                self.price = 180
+        elif self.membership == self.MEMBERSHIP_GOLD:
+            if self.period == self.MONTHLY_PERIOD:
+                self.price = 30
+            elif self.period == self.QUARTERLY_PERIOD:
+                self.price = 75
+            elif self.period == self.ANNUALLY_PERIOD:
+                self.price = 270
+
+        super().save(*args, **kwargs)
